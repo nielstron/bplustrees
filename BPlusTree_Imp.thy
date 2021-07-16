79d9d4d45c6277c4412c1ff64305dcae349d9cd0
Niels Mündler <n.muendler@tum.de>
2021-07-16 11:03:49 +0200

    Move things around

--- BPlusTree_Imp.thy
+++ BPlusTree_Imp.thy
@@ -126,7 +126,7 @@ fun leaf_nodes_assn :: "nat \<Rightarrow> 'a bplustree list \<Rightarrow> 'b btn
 
 
 fun inner_nodes_assn :: "nat \<Rightarrow> 'a bplustree \<Rightarrow> 'b btnode ref \<Rightarrow> 'b btnode ref option \<Rightarrow> 'b btnode ref option \<Rightarrow> assn" where
-  "inner_nodes_assn k (LNode xs) a r z = emp" |
+  "inner_nodes_assn k (LNode xs) a r z = \<up>(r = Some a)" |
   "inner_nodes_assn k (Node ts t) a r z = 
  (\<exists>\<^sub>A tsi ti tsi' tsi'' rs.
       a \<mapsto>\<^sub>r Btnode tsi ti
@@ -150,94 +150,9 @@ lemma butlast_double_Cons: "butlast (x#y#xs) = x#(butlast (y#xs))"
 lemma last_double_Cons: "last (x#y#xs) = (last (y#xs))"
   by auto
 
-lemma entails_preI: "(\<And>h. h \<Turnstile> P \<Longrightarrow> P \<Longrightarrow>\<^sub>A Q) \<Longrightarrow> P \<Longrightarrow>\<^sub>A Q"
-  unfolding entails_def
-  by auto
-
-lemma ent_true_drop_true: 
-  "P*true\<Longrightarrow>\<^sub>AQ*true \<Longrightarrow> P*R*true\<Longrightarrow>\<^sub>AQ*true"
-  using assn_aci(10) ent_true_drop(1) by presburger
 
 declare last.simps[simp del] butlast.simps[simp del]
 declare mult.left_assoc[simp add]
-
-(* TODO *)
-lemma rem_true: "P*true \<Longrightarrow>\<^sub>A Q*true \<Longrightarrow> P \<Longrightarrow>\<^sub>AQ*true"
-  using enttD enttI_true by blast
-
-lemma ent_wandI2:
-  assumes IMP: "P \<Longrightarrow>\<^sub>A (Q -* R)"
-  shows "Q*P \<Longrightarrow>\<^sub>A R"
-  using assms
-  unfolding entails_def 
-(*  by (meson assms ent_fwd ent_mp ent_refl fr_rot mod_frame_fwd)*)
-proof (clarsimp, goal_cases)
-  case (1 h as)
-  then obtain as1 as2 where "as = as1 \<union> as2" "as1 \<inter> as2 = {}" "(h,as1) \<Turnstile> Q" "(h,as2) \<Turnstile> P"
-    by (metis mod_star_conv prod.inject)
-  then have "(h,as2) \<Turnstile> (Q-*R)"
-    by (simp add: "1"(1))
-  then have "(h,as1\<union>as2) \<Turnstile> Q * (Q-*R)"
-    by (simp add: \<open>(h, as1) \<Turnstile> Q\<close> \<open>as1 \<inter> as2 = {}\<close> star_assnI)
-  then show ?case 
-    using \<open>as = as1 \<union> as2\<close> ent_fwd ent_mp by blast
-qed
-
-lemma ent_wand: "(P \<Longrightarrow>\<^sub>A (Q -* R)) = (Q*P \<Longrightarrow>\<^sub>A R)"
-  using ent_wandI2 ent_wandI by blast
-
-lemma wand_ent_trans:
-  assumes "P' \<Longrightarrow>\<^sub>A P"
-      and "Q \<Longrightarrow>\<^sub>A Q'"
-    shows "P -* Q \<Longrightarrow>\<^sub>A P' -* Q'"
-  by (meson assms(1) assms(2) bplustree.ent_wand ent_frame_fwd ent_refl ent_trans)
-
-lemma wand_elim: "(P -* Q) * (Q -* R) \<Longrightarrow>\<^sub>A (P -* R)"
-  by (metis ent_wand ent_frame_fwd ent_mp ent_refl star_assoc)
-
-lemma emp_wand_same: "emp \<Longrightarrow>\<^sub>A (H -* H)"
-  by (simp add: ent_wandI)
-
-lemma emp_wand_equal: "(emp -* H) = H"
-  apply(intro ent_iffI)
-  apply (metis ent_mp norm_assertion_simps(1))
-  by (simp add: ent_wandI)
-
-lemma pure_wand_equal: "P \<Longrightarrow> (\<up>(P) -* H) = H"
-  by (simp add: emp_wand_equal)
-
-lemma pure_wand_ent: "(P \<Longrightarrow> (H1 \<Longrightarrow>\<^sub>A H2)) \<Longrightarrow> H1 \<Longrightarrow>\<^sub>A \<up>(P) -* H2"
-  by (simp add: ent_wand)
-
-lemma "\<up>(P \<longrightarrow> Q) \<Longrightarrow>\<^sub>A (\<up>(P) -* \<up>(Q))"
-  by (simp add: pure_wand_ent)
-
-
-
-find_theorems "(-*)"
-lemma assumes "P \<Longrightarrow>\<^sub>A Q*true"
-  shows "P \<Longrightarrow>\<^sub>A Q*(Q-*P)"
-  using assms
-  unfolding entails_def 
-  find_theorems "_ \<Turnstile> _ * _"
-proof (clarsimp, goal_cases)
-  case (1 h as)
-  then obtain as1 as2 where "as = as1 \<union> as2" "as1 \<inter> as2 = {}" "(h,as1) \<Turnstile> Q" "(h,as2) \<Turnstile> true"
-    by (metis mod_star_conv prod.inject)
-  moreover have "(h,as1\<union>as2) \<Turnstile> P"
-    using "1"(2) calculation(1) by auto
-  have "Q*(Q-*P) \<Longrightarrow>\<^sub>A P"
-    by (simp add: ent_mp)
-  have "(h, as2) \<Turnstile> Q-*P"
-    thm wand_assnI
-  apply (rule wand_assnI)
-    using \<open>(h, as2) \<Turnstile> true\<close> apply force
-  proof (goal_cases)
-    case (1 h' as')
-    then show ?case sorry
-  qed
-oops
-
 lemma bplustree_leaf_nodes:
   "bplustree_assn k t ti r z * true \<Longrightarrow>\<^sub>A leaf_nodes_assn k (leaf_nodes t) r z * true"
 proof(induction arbitrary: r rule: bplustree_assn.induct)
@@ -325,7 +240,154 @@ and B'="leaf_nodes_assn k (concat (map (\<lambda>a. leaf_nodes (fst a)) tss) @ l
 qed
 declare last.simps[simp add] butlast.simps[simp add]
 declare mult.left_assoc[simp del]
+
+declare last.simps[simp del] butlast.simps[simp del]
+declare mult.left_assoc[simp add]
+lemma bplustree_leaf_nodes:
+  "bplustree_assn k t ti r z \<Longrightarrow>\<^sub>A leaf_nodes_assn k (leaf_nodes t) r z * inner_nodes_assn k t ti r z"
+  oops
+(*
+proof(induction arbitrary: r rule: bplustree_assn.induct)
+  case (1 k xs a r z)
+  then show ?case
+    by (sep_auto)
+next
+  case (2 k ts t a r z ra)
+  then show ?case
+    apply(sep_auto)
+  proof (goal_cases)
+    case (1 a b ti tsi' rs)
+    have *: "
+        length tsi's = length rss \<Longrightarrow>
+        length rss = length tss \<Longrightarrow>
+        set tsi's \<subseteq> set tsi' \<Longrightarrow>
+        set rss \<subseteq> set rs \<Longrightarrow>
+        set tss \<subseteq> set ts \<Longrightarrow>
+       bplustree_assn k t ti (last (ra # rss)) z * 
+       blist_assn k tss
+        (zip (zip (subtrees tsi's) (zip (butlast (ra # rss)) rss)) (separators tsi's)) * true \<Longrightarrow>\<^sub>A
+       leaf_nodes_assn k (concat (map (leaf_nodes \<circ> fst) tss) @ leaf_nodes t) ra z * true"
+      for rss tsi's tss
+    proof (induct arbitrary: ra rule: list_induct3)
+      case (Nil r)
+      then show ?case
+        apply sep_auto
+        using 2(1)[of ti r]
+      apply (simp add: last.simps butlast.simps)
+      done
+    next
+      case (Cons subsepi tsi's subleaf rss subsep tss r)
+      show ?case
+        apply (sep_auto
+                simp add: butlast_double_Cons last_double_Cons)
+        apply(subst prod_assn_def)
+        apply(simp split!: prod.splits add: mult.left_assoc)
+        apply(subst leaf_nodes_assn_aux_append)
+        apply simp
+        apply(inst_ex_assn subleaf)
+      proof (goal_cases)
+        case (1 sub sep)
+        have "(sub,sep) \<in> set ts"
+          using "1" Cons.prems(3) by force
+        moreover have "set tsi's \<subseteq> set tsi' \<and> set rss \<subseteq> set rs \<and> set tss \<subseteq> set ts"
+          by (meson Cons.prems set_subset_Cons subset_trans)
+        moreover obtain temp1 temp2 where "((fst subsepi, (temp1:: 'b btnode ref option), subleaf), (temp2::'b)) \<in> set [((fst subsepi, temp1, subleaf), temp2)]"
+          by auto
+        ultimately  show ?case
+          using
+           Cons(3)[of subleaf]
+           "2.IH"(2)[of "(sub,sep)"
+                "((fst subsepi, (temp1, subleaf)),temp2)" "[((fst subsepi, (temp1, subleaf)),temp2)]"
+                "fst subsepi" "(temp1, subleaf)" temp1 subleaf r]
+          apply auto
+          thm mult.commute
+          thm star_aci
+          apply(subst mult.commute)
+          supply R=ent_star_mono_true[where
+A="bplustree_assn k sub (the (fst subsepi)) r subleaf * true" and A'="leaf_nodes_assn k (leaf_nodes sub) r subleaf"
+and B="bplustree_assn k t ti (last (subleaf # rss)) z *
+    A_assn sep (snd subsepi) *
+    blist_assn k tss
+     (zip (zip (subtrees tsi's) (zip (butlast (subleaf # rss)) rss)) (separators tsi's)) * true"
+and B'="leaf_nodes_assn k (concat (map (\<lambda>a. leaf_nodes (fst a)) tss) @ leaf_nodes t) subleaf z"
+          ,simplified]
+          thm R
+          apply(rule R)
+          subgoal
+            by simp
+          subgoal
+            apply(subst mult.commute, simp)
+            apply(rule ent_true_drop_true)
+            apply(subst mult.commute, simp)
+            done
+      done
+      qed
+    qed
+    show ?case
+      apply(rule entails_preI)
+        using 1 apply (auto dest!: mod_starD list_assn_len)
+        using *[of tsi' rs ts, simplified]
+        by (smt (z3) assn_aci(10) assn_times_comm ent_true_drop(1))
+  qed
+qed
+declare last.simps[simp add] butlast.simps[simp add]
+declare mult.left_assoc[simp del]
 thm bplustree_assn.simps
+*)
+
+subsection "Iterator"
+
+partial_function (heap) leaf_iter_init :: "'b btnode ref \<Rightarrow> 'b btnode ref Heap"
+  where
+    "leaf_iter_init p = do {
+  node \<leftarrow> !p;
+  (case node of
+    Btleaf _ _ \<Rightarrow> do { return p } |
+    Btnode tsi ti \<Rightarrow> do {
+        s \<leftarrow> pfa_get tsi 0;
+        let (sub,sep) = s in do { 
+          leaf_iter_init (the sub)
+        }
+  }
+)}"
+
+declare last.simps[simp del] butlast.simps[simp del]
+lemma obtain_first_leaf_rule:
+  assumes "k > 0" "root_order k t"
+  shows "<bplustree_assn k t ti r z>
+  leaf_iter_init ti
+  <\<lambda>u. bplustree_assn k t ti r z * \<up>(Some u = r)>\<^sub>t"
+  using assms
+proof(induction t arbitrary: ti z)
+  case (LNode x)
+  then show ?case
+    apply(subst leaf_iter_init.simps)
+    apply (sep_auto dest!: mod_starD)
+    done
+next
+  case (Node ts t)
+  then obtain sub sep tts where Cons: "ts = (sub,sep)#tts"
+    apply(cases ts) by auto
+  then show ?case 
+    apply(subst leaf_iter_init.simps)
+    apply(rule hoare_triple_preI)
+    apply (sep_auto simp add: last.simps butlast.simps)
+    subgoal for a b aa ba ti tsi' rs ab bb tiaa tsi'a rsa ac bc
+    using "Node.IH"(1)[of "(sub,sep)" sub]
+    apply sep_auto
+    sorry
+  done
+qed
+declare last.simps[simp add] butlast.simps[simp add]
+
+lemma obtain_iter_rule:
+  assumes "k > 0" "root_order k t"
+  shows "<bplustree_assn k t ti r z>
+  leaf_iter_init ti
+  <\<lambda>u. leaf_nodes_assn k (leaf_nodes t) (Some u) z>\<^sub>t"
+  using assms
+  using bplustree_leaf_nodes[of k t ti r z]
+  by (sep_auto heap add: obtain_first_leaf_rule)
 
 definition leaf_iter_next where
 "leaf_iter_next r = do {
@@ -351,17 +413,8 @@ leaf_iter_next n
   done
 
 
-lemma "<leaf_nodes_assn k ((LNode (u#us))#xs) r z>
-      leaf_iter_next r <\<lambda>(v, n). leaf_nodes_assn k xs n z * A_assn u v * true>"
-  apply(subst leaf_iter_next_def)
-  apply(cases r)
-  apply(sep_auto)
-  apply simp
-  apply(intro normalize_rules)
-  apply(rule hoare_triple_preI)
-  apply(sep_auto dest!: mod_starD list_assn_len)
 
-  done
+
 
 end
 
